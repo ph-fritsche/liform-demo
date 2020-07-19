dde12242aab84ecac690eb36eefb5bed12f96e36
Philipp Fritsche <ph.fritsche@gmail.com>
2020-07-19 13:38:37 +0200

    Update liform-react-final integration

--- assets/js/liform.jsx
+++ assets/js/liform.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
 import ReactOnRails from 'react-on-rails'
-import Liform from 'liform-react-final'
-import { renderField } from 'liform-react-final/dist/field'
+import { Liform, DefaultTheme, renderField, finalizeName } from 'liform-react-final'
 import { Field } from 'react-final-form'
 
 const demoRenderField = (props) => {
     return <div style={{border: "1px solid grey", padding: "5px", margin: "5px"}}>
         <div><small>name: {props.name}</small></div>
         <div><code>schema: { JSON.stringify({widget: props.schema.widget, type: props.schema.type, format: props.schema.format}) }</code></div>
-        { props.name &&
-            <Field name={props.name} render={({input: {value}}) => 
+        { props.schema.type &&
+            <Field name={finalizeName(props.name)} render={({input: {value}}) => 
                 <div><code>value: { JSON.stringify(value) }</code></div>
             }/>
         }
@@ -18,11 +17,13 @@ const demoRenderField = (props) => {
 }
 
 const DemoForm = ({verboseFields, ...props}) => {
-    if (verboseFields) {
-        props.renderField = demoRenderField
-    }
+    const render = verboseFields ? {field: demoRenderField} : undefined
 
-    return <Liform {...props}/>
+    return (
+        <Liform theme={DefaultTheme} render={render} {...props}>
+            <header>{ props => <div><code>Initial values: { JSON.stringify(props.initialValues._) }</code></div> }</header>
+        </Liform>
+    )
 }
 
 ReactOnRails.register({ DemoForm })
